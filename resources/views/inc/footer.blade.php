590215a3f3b911d205b3e9f3d9fa30fdb4e750bc
SuicV <drissoulhbibcontact@gmail.com>
2019-12-05 14:04:18 +0100

    create first version of header and footer

---
+++ resources/views/inc/footer.blade.php
@@ -0,0 +1,38 @@
+@section("footer")
+  <footer id="main-footer" class="py-4">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+          <h4 class="footer-section-title">Location</h4>
+          <div class="mapouter" style="width:100%; heigth:800px">
+            <div class="gmap_canvas" style="width:100%;">
+              <iframe style="width=100%;heigth:100%;" id="gmap_canvas" src="https://maps.google.com/maps?q=fst%20marrakech&t=&z=15&ie=UTF8&iwloc=&output=embed" frameborder="0"
+               scrolling="yes" marginheight="0" marginwidth="0"></iframe>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <h4 class="footer-section-title">Informations</h4>
+          <ul class="list-style-none">
+            <li><a href="">Method de payment</a></li>
+            <li><a href="">Nos Regle</a></li>
+            <li><a href="">Proccess de payment</a></li>
+            <li><a href="">A propos</a></li>
+          </ul>
+        </div>
+        <div class="col-md-4">
+          <h4 class="footer-section-title">Contacter nous</h4>
+          <form action="http://www.el-gamer.com" method="get">
+            <div class="form-group">
+              <input type="text" name="nom" id="contact-name" class="form-control" placeholder="Nom">
+            </div>
+            <div class="form-group">
+              <input type="email" name="email" id="contact-email" class="form-control" placeholder="email">
+            </div>
+            <button type="submit" class="btn btn-primary">Continue</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </footer>
+@show
---
+++ resources/views/inc/header.blade.php
@@ -0,0 +1,48 @@
+@section('header')
+	<section class="container">
+		<div class="row py-2">
+			<div class="col-12 col-sm-6">
+				<ul class="inline-list m-0 p-0" id="h-social-links">
+					<li><a href="#"><i class="top-header-icons fa fa-facebook-square"></i></a></li>
+					<li><a href="#"><i class="top-header-icons fa fa-twitter-square"></i></a></li>
+					<li><a href="#"><i class="top-header-icons fa fa-instagram"></i></a></li>
+					<li><a href="#" class="text-dark"><i class="top-header-icons fa fa-map"></i></a></li>
+				</ul>
+			</div>
+			<div class="col-12 col-sm-6 text-right">
+				<ul class="inline-list p-0 m-0">
+					<li class="d-block d-md-inline"><i class="top-header-icons fa fa-mobile"></i><span>Mobile <span class="phone-number">06-5489-96</span></span></li>
+					<li class="d-block d-md-inline"><i class="top-header-icons fa fa-phone"></i><span>Service de client <span class="phone-number">06-5489-96</span></span></li>
+				</ul>
+			</div>
+		</div>
+	</section>
+	<nav class="navbar navbar-expand-sm navbar-dark navbar-bg">
+		<div class="container px-0">
+			<a class="navbar-brand" href="{{ route('home') }}">El-Gamer</a>
+			<div class="navbar-toggler d-lg-none" role="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+					aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-drop-button"></span>
+				<span class="navbar-drop-button"></span>
+				<span class="navbar-drop-button"></span>
+			</div>
+			<div class="collapse navbar-collapse justify-content-between" id="collapsibleNavId">
+				<div></div>
+				<ul class="navbar-nav mt-2 mt-lg-0">
+					<li class="nav-item active">
+						<a class="nav-link" href="{{ route('home') }}">Acceuil</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="{{ route('produits') }}">Produits</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="{{ route('promos') }}">Promos</a>
+					</li>
+				</ul>
+				<div class="">
+					<span class="text-light">Pannier <i class="fa fa-shopping-bag" aria-hidden="true"></i> <span id="pannier-sum">0 DH</span></span>
+				</div>
+			</div>
+		</div>
+	</nav>
+@show
