d7477d2222b1bda3e73e6a69d20734577f8bcaaa
SuicV <drissoulhbibcontact@gmail.com>
2019-12-09 20:08:29 +0100

    make categories and sub categories system

---
+++ app/Category.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    protected $fillable = ["name","parent_cat"];
+
+    public function subCategories(){
+        return $this->hasMany("App\Category","parent_cat");
+    }
+    public function parentCategory(){
+        return $this->belongsTo("App\Category","parent_cat");
+    }
+}
