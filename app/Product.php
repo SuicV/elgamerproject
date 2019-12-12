a178127fe001b0d8b14b9567da3528d2f4e51dee
SuicV <drissoulhbibcontact@gmail.com>
2019-12-12 13:30:32 +0100

    add cast type to attributes column

--- app/Product.php
+++ app/Product.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     protected $fillable = ["title", "description", "image", "price", "attributes", "category_id"];
+    protected $casts = ["attributes"=>"Array"];
     public function category(){
         return $this->belongsTo("App\Category","category_id");
     }
