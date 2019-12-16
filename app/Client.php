1cccac5ec2399732e367859506163110ec38e0f3
SuicV <drissoulhbibcontact@gmail.com>
2019-12-16 23:16:30 +0100

    cast rib type

--- app/Client.php
+++ app/Client.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Client extends Model
 {
     protected $fillable = ["fullName","email","phone","address","rib"];
+    protected $casts = ["rib"=>"string"];
     public function orders (){
         return $this->hasMany("\App\Order","client_id");
     }
