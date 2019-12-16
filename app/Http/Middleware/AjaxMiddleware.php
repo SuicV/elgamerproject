00af02ed83bae8df97d4cb7b9cf0456b65ff8eaf
SuicV <drissoulhbibcontact@gmail.com>
2019-12-16 20:25:04 +0100

    add middlewares

--- app/Http/Middleware/AjaxMiddleware.php
+++ app/Http/Middleware/AjaxMiddleware.php
@@ -4,20 +4,19 @@ namespace App\Http\Middleware;
 
 use Closure;
 
-class PurchaseMiddleware
+class AjaxMiddleware
 {
-    private $redirectTo = "produits";
     /**
      * Handle an incoming request.
-     * redirect the request to products page if chart is empty
+     *
      * @param  \Illuminate\Http\Request  $request
      * @param  \Closure  $next
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
-        if(session()->get("chart.totalPrice",0) === 0){
-            return redirect(route($this->redirectTo));
+        if(!$request->ajax()){
+            return response("",400);
         }
         return $next($request);
     }
