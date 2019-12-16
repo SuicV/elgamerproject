dfdb2ce604f9cce9f8ab92870987972d5c09e6f5
SuicV <drissoulhbibcontact@gmail.com>
2019-12-16 21:56:46 +0100

    add middlewares

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -61,6 +61,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'purchase' => \App\Http\Middleware\PurchaseMiddleware::class,
+        'isAjax' => \App\Http\Middleware\AjaxMiddleware::class
     ];
 
     /**
