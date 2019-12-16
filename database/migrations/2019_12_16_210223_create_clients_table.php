0dd0b689f2a2d85dca0f2954132d1602407774b2
SuicV <drissoulhbibcontact@gmail.com>
2019-12-16 22:19:49 +0100

    create migrations for clients and orders

---
+++ database/migrations/2019_12_16_210223_create_clients_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string("fullName",150);
+            $table->string("email",255)->unique();
+            $table->string("phone",15);
+            $table->string("address",512);
+            $table->string("rib",20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
