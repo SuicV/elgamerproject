6e1097bd01af2966cf495b135265fbfad4cef7f6
SuicV <drissoulhbibcontact@gmail.com>
2019-12-17 20:02:07 +0100

    add fillable column

--- app/Order.php
+++ app/Order.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    protected $fillable = ["client_id","products","total_price"];
+    protected $fillable = ["client_id","order_code","products","total_price"];
     protected $casts = ["products"=>"array"];
     public function client(){
         return $this->belongsTo("\App\Client", "client_id");
