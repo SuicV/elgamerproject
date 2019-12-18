71741f5b229cde45100e08d525361492615e256f
SuicV <drissoulhbibcontact@gmail.com>
2019-12-18 22:35:04 +0100

    create mixing for products

--- webpack.mix.js
+++ webpack.mix.js
@@ -16,4 +16,6 @@ mix.js('resources/js/products/welcome.js', 'public/js/products')
     .js("resources/js/chart/welcome.js", "public/js/chart");
 // sass mixings
 mix.sass('resources/sass/welcome.scss', 'public/css')
-    .sass('resources/sass/contact.scss', 'public/css');
+    .sass('resources/sass/contact.scss', 'public/css')
+    .sass('resources/sass/produits/details.scss', 'public/css/produits')
+    .sass('resources/sass/produits/welcome.scss', 'public/css/produits');
