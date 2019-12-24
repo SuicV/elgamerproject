6d88bb162d4bdbbf104327b93505d02a5a1d59ec
SuicV <drissoulhbibcontact@gmail.com>
2019-12-24 21:36:16 +0100

    install relations between discounts and products tables

--- app/Discount.php
+++ app/Discount.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Discount extends Model
 {
-    //
+    protected $fillable = ["price"];
+    public function product (){
+        return $this->hasOne("App\Proudct", "discount_id");
+    }
 }
--- app/Product.php
+++ app/Product.php
@@ -6,9 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    protected $fillable = ["title", "description", "image", "price", "attributes", "category_id"];
+    protected $fillable = ["title", "description", "image", "price", "attributes", "category_id","discount_id"];
     protected $casts = ["attributes"=>"Array"];
     public function category(){
         return $this->belongsTo("App\Category","category_id");
+    }
+    public function discount(){
+        return $this->belongsTo('App\Discount','discount_id');
     }
 }
