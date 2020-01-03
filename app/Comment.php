99da9e27e61b355cfd0c01b7f065d8d8f43e45f0
SuicV <drissoulhbibcontact@gmail.com>
2020-01-03 20:33:18 +0100

    create rating column in comments table

--- app/Comment.php
+++ app/Comment.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    protected $fillable = ["name","email","comment","product_id"];
+    protected $fillable = ["name","email","comment","product_id","rating"];
     public function product(){
         return $this->belongsTo('App\Product','product_id');
     }
