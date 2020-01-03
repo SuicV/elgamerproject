fdaeccefc1483ca1d1ce315d5745968b609160b9
SuicV <drissoulhbibcontact@gmail.com>
2020-01-03 20:42:08 +0100

    upgrade to font-awesom 5

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -9,7 +9,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <!-- Font-Awesom CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
     <link href="https://fonts.googleapis.com/css?family=Press+Start+2P|Roboto:300,400,700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="/css/{{ $css }}.css">
   </head>
