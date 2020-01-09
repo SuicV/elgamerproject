e10807c594c426e80b9253e36f6f2548d4e6dac7
SuicV <drissoulhbibcontact@gmail.com>
2020-01-09 19:44:16 +0100

    update register route

--- routes/web.php
+++ routes/web.php
@@ -30,7 +30,7 @@ Route::post('/contacter-nous', "ContactController@store")->name("contact-us.stor
 Route::put("/comment", "CommentsController@store")->name("comments.store");
 Route::post("/comment/{id}", "CommentsController@get")->name("comments.get")->where(["id"=>"^[0-9]+$"]);
 // register routes
-Route::get("/crée-compte", "Auth\RegisterController@get")->name("register.get");
-Route::post("/crée-compte", "Auth\RegisterController@store")->name("register.store");
+Route::get("/inscription", "Auth\RegisterController@get")->name("register.get");
+Route::post("/inscription", "Auth\RegisterController@store")->name("register.store");
 // login routes
 Route::get("/se-connecter", "Auth\LoginController@get")->name("login.get");
