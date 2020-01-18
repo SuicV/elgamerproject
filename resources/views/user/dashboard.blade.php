9a925cd82b4e8df87c552275f9ab1c850dc31814
SuicV <drissoulhbibcontact@gmail.com>
2020-01-18 12:45:28 +0100

    change routes

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -6,9 +6,10 @@
     <section class="container pb-5">
         <div class="row">
             <div class="col-md-7">
-                <form action="{{ route('dashboard.store') }}" method="post">
+                <form id="form-basics" action="{{ route('dashboard.store') }}" method="post">
                     <p>Information Générale</p>
                     @csrf
+                    <input type="hidden" name="type" value="basics">
                     <div class="form-group">
                         <label for="name">Nom Complet</label>
                         <input type="text" value="{{ Auth::user()->name }}" name="name" id="name" class="form-control">
@@ -21,9 +22,10 @@
                         <button class="btn btn-outline-success disabled">Changer</button>
                     </div>
                 </form>
-                <form action="{{ route('dashboard.store') }}" method="post">
+                <form id="form-password" action="{{ route('dashboard.store') }}" method="post">
                     <p>changer le password</p>
                     @csrf
+                    <input type="hidden" name="type" value="password">
                     <div class="form-group">
                         <label for="oldpassword">Ancien Mot de pass</label>
                         <input type="password" name="old_password" id="oldpassword" class="form-control">
@@ -42,14 +44,23 @@
                 </form>
             </div>
             <div class="col-md-3">
-                <form action="{{ route('dashboard.store') }}" method="post">
+                <form id="form-image" action="{{ route('dashboard.store') }}" enctype="multipart/form-data" method="post">
                     @csrf
+                    <input type="hidden" name="type" value="image">
+                    <div class="form-group text-center">
+                        <label for="image" class="d-flex justify-content-center border-rounded">
+                            @if(auth()->user()->image)
+                                <img class="img-fluid img-thumbnail rounded-circle" src="{{asset('imgs/users/'.auth()->user()->image)}}" alt="image de profile">
+                            @endif
+                        </label>
+                        <input type="file" name="image" id="image" class="d-none">
+                    </div>
                     <div class="form-group">
-                        <label for="image">image de profile</label>
-                        <input type="file"name="image" id="image" class="d-none">
+                        <button class="btn btn-outline-success" type="submit">Changer la photo</button>
                     </div>
                 </form>
             </div>
         </div>
     </section>
 @endsection
+@section("script")
